a1a5827cca855139a0ee95ed86f98d48acff72e0
Tueloper <ozurumbatochukwu@yahoo.com>
2019-06-05 18:18:13 +0100

    Dashboard Set up

--- routes/index.js
+++ routes/index.js
@@ -2,31 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET Static Pages. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'KodeHauz Training Portal' });
 });
-router.get('/KodeHauzHub', function(req, res, next) {
+router.get('/KodeHauzHub', function (req, res, next) {
   res.render('KodeHauzHub', { title: 'KodeHauz Training Portal' });
 });
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('login', { title: 'KodeHauz Training Portal' });
 });
-router.get('/signUp', function(req, res, next) {
+router.get('/signUp', function (req, res, next) {
   res.render('signUp', { title: 'KodeHauz Training Portal' });
 });
-router.get('/trainer', function(req, res, next) {
+router.get('/trainer', function (req, res, next) {
   res.render('trainer', { title: 'KodeHauz Training Portal' });
 });
-router.get('/trainings', function(req, res, next) {
+router.get('/trainings', function (req, res, next) {
   res.render('trainings', { title: 'KodeHauz Training Portal' });
 });
 
 /* GET admin pages. */
-router.get('/admin/categories', function(req, res, next) {
+router.get('/admin/categories', function (req, res, next) {
   res.render('admin/categories',  { title: 'KodeHauz Training Portal' });
 });
 
-router.get('/admin/dashboard', function(req, res, next) {
-  res.render('admin/dashboard', { title: 'KodeHauz Training Portal' });
+router.get('/admin/dashboard', function (req, res, next) {
+  res.render('dashboard', { title: 'KodeHauz Training Portal' });
 });
+router.get('/admin/login', function (req, res, next) {
+  res.render('dashboard-login', { title: 'KodeHauz Admin Dashboard Login' });
+});
+
 module.exports = router;
