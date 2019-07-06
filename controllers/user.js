d01a0d1a5cdf64b90dfa59f514f88be2b208f3aa
Tueloper <ozurumbatochukwu@yahoo.com>
2019-07-06 01:04:55 +0100

    set profile details

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const Category = require('../models/embedded/categories')
-const Skills = require('../models/embedded/skill_level');
+const emSkills = require('../models/embedded/skill_level');
 const Interest_Area = require('../models/embedded/interest_area');
 const Trainee = require('../models/trainee_profile')
 const sharp = require('sharp')
@@ -8,10 +8,10 @@ const Datauri = require('datauri')
 const path = require('path')
 const dUri = new Datauri();
 const Education = require('../models/trainee_education')
-const Skill = require('../models/trainee_skill')
+const train_Skill = require('../models/trainee_skill')
 const Internet = require('../models/internet')
 const Guardian = require('../models/trainee_guardian')
-const upload = require('../config/upload')
+// const upload = require('../config/upload')
 require('../config/cloudinary')
 
 
@@ -126,8 +126,8 @@ module.exports = {
                 const education = await Education.findOne({ trainee_id })
 
                 //get trainee skills
-                const skill = await Skill.findOne({ trainee_id })
-
+                const skill = await train_Skill.findOne({ trainee_id })
+                
                 //get guardian
                 const guardian = await Guardian.findOne({ trainee_id })
 
@@ -137,13 +137,14 @@ module.exports = {
                 //Getting Id's
                 const skill_id = skill.level_id
                 const interest_id = skill.interest_id
-
+                // return console.log(interest_id)
                 //Getting skills
-                const skillSet = await Skill.findOne(skill_id)
+                const skillSet = await emSkills.findOne(skill_id)
+                
 
                 //Getting Interest-area
                 const interestSet = await Interest_Area.findOne(interest_id)
-              
+                // return console.log(skillSet)
                 res.status(200).render('trainee_profile', {
                     title: 'Training Registration zone',
                     trainee,
