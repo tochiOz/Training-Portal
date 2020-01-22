fab2259a46d494f9d1e0720e8b95401f1921c7d3
Tueloper <ozurumbatochukwu@yahoo.com>
2020-01-22 15:13:21 +0100

    Config keys sent

---
+++ config/ci.js
@@ -0,0 +1,11 @@
+module.exports = {
+	MONGO_URL: process.env.MONGO_URL,
+	cookieKey: process.env.COOKIE_KEY,
+	redisUrl: process.env.redisUrl,
+	SECRET: process.env.SECRET,
+	PORT: process.env.PORT,
+	CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+	CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+	CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+	RECAPTCHA_SECRET: process.env.RECAPTCHA_SECRET
+};
