4880fb23a7b6f3b0370a18894edd45ba233086c1
Tueloper <ozurumbatochukwu@yahoo.com>
2020-01-31 15:51:48 +0100

    app not running

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+	MONGO_URL: 'mongodb://127.0.0.1:27017/portal_dev_db',
+	cookieKey: 'port12al3123',
+	redisUrl: 'redis://127.0.0.1:6379',
+	SECRET: 'podsial9873',
+	PORT: '5000',
+	CLOUDINARY_CLOUD_NAME: 'kodehauz',
+	CLOUDINARY_API_KEY: '485175117139788',
+	CLOUDINARY_API_SECRET: 'bno54Ih3b780IKs8qLOOnwpzzU0',
+	RECAPTCHA_SECRET: '6Lfxf68UAAAAAM-88HIDjuAzDQ7i3-DI2dnmgUFh'
+};
